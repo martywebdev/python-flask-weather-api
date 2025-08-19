8597c349e1ddde37b1fea9bd6d2d0b493436cc5e
Marty <martyumlas@gmail.com>
2025-08-19 22:12:30 +0800

    build rest api

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/api/v1/<station>/<date>')
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+    return render_template('about.html')
+if __name__ == "__main__": # this is will only run directly not when imported remember that
+    app.run(debug=True)
